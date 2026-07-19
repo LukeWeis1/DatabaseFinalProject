55d172a5baab120554513e098980941bf092297a
LukeWeis1 <lukeweis@outlook.com>
2026-07-19 19:36:47 -0400

    staff member sample data

---
+++ sampleData.sql
@@ -0,0 +1,5 @@
+INSERT INTO StaffMember (FirstName, LastName, DateOfBirth, IsManager, Password, CompanyPhoneNumber, CompanyEmailAddress, CreatedBy)
+VALUES 
+('Admin', 'User', '1980-01-01', TRUE, 'admin_hash', '555-0000', 'admin@rentals.com', NULL),
+('Luke', 'Weis', '2005-06-18', FALSE, 'luke_hash', '713-3626', 'luke@rentals.com', 1);
+
