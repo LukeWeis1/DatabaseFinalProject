2af4638d882e32c18573ce1d11b8d7cb0e0b7686
LukeWeis1 <lukeweis@outlook.com>
2026-07-19 18:38:32 -0400

    Added Customers and related tables

--- createSchema.sql
+++ createSchema.sql
@@ -21,3 +21,27 @@ CREATE TABLE Vehicle (
 	CreatedBy INT,
     FOREIGN KEY (CreatedBy) REFERENCES StaffMember(EmployeeID)
 );
+
+CREATE TABLE Customer (
+    DriversLicenseNumber VARCHAR(50) PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    CardNumber VARCHAR(255) NOT NULL,
+    CreatedBy INT,
+    FOREIGN KEY (CreatedBy) REFERENCES StaffMember(EmployeeID)
+);
+
+CREATE TABLE CustomerEmails (
+    DriversLicenseNumber VARCHAR(50) NOT NULL,
+    EmailAddress VARCHAR(255) NOT NULL,
+    PRIMARY KEY (DriversLicenseNumber, EmailAddress),
+    FOREIGN KEY (DriversLicenseNumber) REFERENCES Customer(DriversLicenseNumber) ON DELETE CASCADE
+);
+
+CREATE TABLE CustomerPhoneNumbers (
+    DriversLicenseNumber VARCHAR(50) NOT NULL,
+    PhoneNumber VARCHAR(20) NOT NULL,
+    PRIMARY KEY (DriversLicenseNumber, PhoneNumber),
+    FOREIGN KEY (DriversLicenseNumber) REFERENCES Customer(DriversLicenseNumber) ON DELETE CASCADE
+);
